215f5d45753ced3ef755d5d4ab80ec1ad2d52a1d
TehBrian <tehbrian@proton.me>
2023-02-12 01:16:38 -0500

    converter: use Kotlin DSL for build.gradle

---
+++ converter/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("idea")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.im4java:im4java:1.4.0")
+    implementation("com.beust:jcommander:1.82")
+}
+
+java.sourceSets["main"].java {
+    srcDir("src")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "pl.asie.ctif.Main"
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
