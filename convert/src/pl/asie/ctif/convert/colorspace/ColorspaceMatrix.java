bf5e394b543afb260791b5ac582c75fa218a5623
TehBrian <tehbrian@proton.me>
2023-02-14 13:14:11 -0500

    convert: remove unused parent parameter in ColorspaceMatrix

--- convert/src/pl/asie/ctif/convert/colorspace/ColorspaceMatrix.java
+++ convert/src/pl/asie/ctif/convert/colorspace/ColorspaceMatrix.java
@@ -6,24 +6,20 @@ public class ColorspaceMatrix extends Colorspace {
   private final float[] matrixToRGB;
 
   protected ColorspaceMatrix(float[] matrix) {
-    this(null, matrix);
-  }
-
-  protected ColorspaceMatrix(Colorspace parent, float[] matrix) {
     this.parent = null;
     this.matrixFromRGB = matrix;
     this.matrixToRGB = new float[9];
     float detInv = 1f / (matrix[0] * matrix[4] * matrix[8] + matrix[1] * matrix[5] * matrix[6] + matrix[2] * matrix[3] * matrix[7]
         - (matrix[2] * matrix[4] * matrix[6] + matrix[0] * matrix[5] * matrix[7] + matrix[1] * matrix[3] * matrix[8]));
-    matrixToRGB[0] = detInv * (matrix[1 * 3 + 1] * matrix[2 * 3 + 2] - matrix[1 * 3 + 2] * matrix[2 * 3 + 1]);
-    matrixToRGB[3] = -detInv * (matrix[1 * 3 + 0] * matrix[2 * 3 + 2] - matrix[1 * 3 + 2] * matrix[2 * 3 + 0]);
-    matrixToRGB[6] = -detInv * (matrix[1 * 3 + 1] * matrix[2 * 3 + 0] - matrix[1 * 3 + 0] * matrix[2 * 3 + 1]);
-    matrixToRGB[1] = -detInv * (matrix[0 * 3 + 1] * matrix[2 * 3 + 2] - matrix[0 * 3 + 2] * matrix[2 * 3 + 1]);
-    matrixToRGB[4] = detInv * (matrix[0 * 3 + 0] * matrix[2 * 3 + 2] - matrix[0 * 3 + 2] * matrix[2 * 3 + 0]);
-    matrixToRGB[7] = detInv * (matrix[0 * 3 + 1] * matrix[2 * 3 + 0] - matrix[0 * 3 + 0] * matrix[2 * 3 + 1]);
-    matrixToRGB[2] = detInv * (matrix[0 * 3 + 1] * matrix[1 * 3 + 2] - matrix[0 * 3 + 2] * matrix[1 * 3 + 1]);
-    matrixToRGB[5] = -detInv * (matrix[0 * 3 + 0] * matrix[1 * 3 + 2] - matrix[0 * 3 + 2] * matrix[1 * 3 + 0]);
-    matrixToRGB[8] = -detInv * (matrix[0 * 3 + 1] * matrix[1 * 3 + 0] - matrix[0 * 3 + 0] * matrix[1 * 3 + 1]);
+    matrixToRGB[0] = detInv * (matrix[3 + 1] * matrix[2 * 3 + 2] - matrix[3 + 2] * matrix[2 * 3 + 1]);
+    matrixToRGB[3] = -detInv * (matrix[3] * matrix[2 * 3 + 2] - matrix[3 + 2] * matrix[2 * 3]);
+    matrixToRGB[6] = -detInv * (matrix[3 + 1] * matrix[2 * 3] - matrix[3] * matrix[2 * 3 + 1]);
+    matrixToRGB[1] = -detInv * (matrix[1] * matrix[2 * 3 + 2] - matrix[2] * matrix[2 * 3 + 1]);
+    matrixToRGB[4] = detInv * (matrix[0 + 0] * matrix[2 * 3 + 2] - matrix[2] * matrix[2 * 3]);
+    matrixToRGB[7] = detInv * (matrix[1] * matrix[2 * 3] - matrix[0 + 0] * matrix[2 * 3 + 1]);
+    matrixToRGB[2] = detInv * (matrix[1] * matrix[3 + 2] - matrix[2] * matrix[3 + 1]);
+    matrixToRGB[5] = -detInv * (matrix[0] * matrix[3 + 2] - matrix[2] * matrix[3]);
+    matrixToRGB[8] = -detInv * (matrix[1] * matrix[3] - matrix[0 + 0] * matrix[3 + 1]);
   }
 
   protected final float[] mmul3(float[] a, float[] b) {
